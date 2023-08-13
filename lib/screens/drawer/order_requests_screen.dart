fbc687f71560af7a4c6c64af97b379e6f3c637db
abubakr.siddique <abubakr.siddiqui@arbisoft.com>
2023-08-13 13:28:45 +0500

    feat: Purchased Items and Requested Orders can now be filtered based on their status

--- lib/screens/drawer/order_requests_screen.dart
+++ lib/screens/drawer/order_requests_screen.dart
@@ -1,18 +1,52 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:oumel/blocs/requests/requests_cubit.dart';
+import 'package:oumel/models/order.dart';
+import 'package:oumel/models/purchase.dart';
 import 'package:oumel/widgets/custom_app_bar_title.dart';
 import 'package:oumel/widgets/order_request_widget.dart';
 
-class OrderRequestsScreen extends StatelessWidget {
+class OrderRequestsScreen extends StatefulWidget {
   const OrderRequestsScreen({super.key});
+
+  @override
+  State<OrderRequestsScreen> createState() => _OrderRequestsScreenState();
+}
+
+class _OrderRequestsScreenState extends State<OrderRequestsScreen> {
+  OrderStatus filterBy = OrderStatus.pending;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const CustomAppBarTitle(title: "Requests"),
+        actions: [
+          Row(
+            children: [
+              const Text(
+                'Filter by : ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              DropdownButton(
+                value: filterBy,
+                items: OrderStatus.values
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(
+                            describeEnum(e),
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (category) => setState(() {
+                  filterBy = category ?? filterBy;
+                }),
+              )
+            ],
+          )
+        ],
       ),
       body: BlocConsumer<RequestsCubit, RequestsState>(
         listener: (context, state) {
@@ -85,18 +119,24 @@ class OrderRequestsScreen extends StatelessWidget {
         },
 
         //* Builder
-        builder: (context, state) => state.requests.isEmpty
-            ? const Center(
-                child: Text("Nothing here for now."),
-              )
-            : ListView.separated(
-                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 36.h),
-                separatorBuilder: (context, index) => Divider(
-                  color: Colors.grey.shade200,
-                ),
-                itemBuilder: (context, index) => OrderRequestWidget(state.requests[index]),
-                itemCount: state.requests.length,
-              ),
+        builder: (context, state) {
+          /* Filter the Items based on the present filter */
+          List<Purchase> filterdItems =
+              state.requests.where((p) => p.order.status == filterBy).toList();
+
+          return state.requests.isEmpty
+              ? const Center(
+                  child: Text("Nothing here for now."),
+                )
+              : ListView.separated(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 36.h),
+                  separatorBuilder: (context, index) => Divider(
+                    color: Colors.grey.shade200,
+                  ),
+                  itemBuilder: (context, index) => OrderRequestWidget(filterdItems[index]),
+                  itemCount: filterdItems.length,
+                );
+        },
       ),
     );
   }
--- lib/screens/drawer/purchase_history_screen.dart
+++ lib/screens/drawer/purchase_history_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -5,27 +6,66 @@ import 'package:oumel/blocs/purchases/purchases_cubit.dart';
 import 'package:oumel/widgets/custom_app_bar_title.dart';
 import 'package:oumel/widgets/purchased_product_widget.dart';
 
-class PurchaseHistoryScreen extends StatelessWidget {
+import '../../models/order.dart';
+import '../../models/purchase.dart';
+
+class PurchaseHistoryScreen extends StatefulWidget {
   const PurchaseHistoryScreen({super.key});
+
+  @override
+  State<PurchaseHistoryScreen> createState() => _PurchaseHistoryScreenState();
+}
+
+class _PurchaseHistoryScreenState extends State<PurchaseHistoryScreen> {
+  OrderStatus filterBy = OrderStatus.pending;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const CustomAppBarTitle(title: "Purchases"),
+        actions: [
+          Row(
+            children: [
+              const Text(
+                'Filter by : ',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              DropdownButton(
+                value: filterBy,
+                items: OrderStatus.values
+                    .map((e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(
+                            describeEnum(e),
+                          ),
+                        ))
+                    .toList(),
+                onChanged: (category) => setState(() {
+                  filterBy = category ?? filterBy;
+                }),
+              )
+            ],
+          )
+        ],
       ),
       body: BlocBuilder<PurchasesCubit, PurchasesState>(
-        builder: (context, state) => state.purchases.isEmpty
-            ? const Center(
-                child: Text("Nothing here for now."),
-              )
-            : ListView.separated(
-                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 36.h),
-                separatorBuilder: (context, index) => const Divider(),
-                itemBuilder: (context, index) =>
-                    PurchasedProductWidget(state.purchases[index]),
-                itemCount: state.purchases.length,
-              ),
+        builder: (context, state) {
+          /* Filter the Items based on the present filter */
+          List<Purchase> filterdItems =
+              state.purchases.where((p) => p.order.status == filterBy).toList();
+
+          return state.purchases.isEmpty
+              ? const Center(
+                  child: Text("Nothing here for now."),
+                )
+              : ListView.separated(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 36.h),
+                  separatorBuilder: (context, index) => const Divider(),
+                  itemBuilder: (context, index) => PurchasedProductWidget(filterdItems[index]),
+                  itemCount: filterdItems.length,
+                );
+        },
       ),
     );
   }
